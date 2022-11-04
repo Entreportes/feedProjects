ec19d559bb7245581f87ce5b5293e463a672d9d1
Lucas Entreportes <lucasentreportes@hotmail.com>
2022-11-04 16:16:11 -0300

    feat:new retirando datas5

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -53,7 +53,7 @@ export function Post({author, publishedAt, content}:PostProps){
             author:"Usuário não conectado",
             comment:newCommentText,
             //trocar data aqui
-            publishedAt: parseISO('2022-11-03T19:00:00Z')
+            publishedAt: parseISO('2022-11-03 19:00:00')
         }
         setComments([...comments,newComment])
         setNewCommentText('')
